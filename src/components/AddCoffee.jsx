ec1ba13b9a5448f7c4e6cfaf857fed31368c6678
SaPa36 <nursapa2001@gmail.com>
2026-01-19 14:51:34 +0600

    create a Add Coffee form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -2,8 +2,74 @@ import React from 'react';
 
 const AddCoffee = () => {
     return (
-        <div>
-            <h2>Add a Coffee</h2>
+        <div className='bg-[#FFFFFF] p-24'>
+            <h2 className='text-5xl text-green-800  '>Add a Coffee</h2>
+            <form >
+            
+            {/* Name and Quantity row */}
+                <div className='md:flex  '>
+                    <div className='md:w-1/2'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Name</legend>
+                            <input type="text" className="input w-full" name='name' placeholder="Name" />
+                        </fieldset>
+                    </div>
+
+                    <div className='md:w-1/2 ml-4'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Available Quantity</legend>
+                            <input type="text" className="input w-full " name='quantity' placeholder="Available Quantity" />
+                        </fieldset>
+                    </div>
+                </div>
+
+                 {/* Supplier and Taste row */}
+                <div className='md:flex  '>
+                    <div className='md:w-1/2'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Supplier</legend>
+                            <input type="text" className="input w-full" name='supplier' placeholder="Supplier" />
+                        </fieldset>
+                    </div>
+
+                    <div className='md:w-1/2 ml-4'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Taste</legend>
+                            <input type="text" className="input w-full " name='taste' placeholder="Taste" />
+                        </fieldset>
+                    </div>
+                </div>
+
+                 {/* Category and Details row */}
+                <div className='md:flex  '>
+                    <div className='md:w-1/2'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Category</legend>
+                            <input type="text" className="input w-full" name='category' placeholder="Category" />
+                        </fieldset>
+                    </div>
+
+                    <div className='md:w-1/2 ml-4'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Details</legend>
+                            <input type="text" className="input w-full " name='details' placeholder="Details" />
+                        </fieldset>
+                    </div>
+                </div>
+
+                {/* Photo URL row */}
+                <div className='md:flex mr-4 '>
+                    <div className='md:w-1/2'>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend text-black">Photo URL</legend>
+                            <input type="text" className="input w-full" name='photo' placeholder="Photo URL" />
+                        </fieldset>
+                    </div>
+
+                </div>
+
+                <input type="submit" value="Add Coffee" className='btn btn-block mt-6 bg-green-800 text-white' />
+            </form>
         </div>
     );
 };
